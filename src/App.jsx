5eb830690d2919632762618203e2dc399f4e4158
Kimbestiga <86374729+Kimbestiga@users.noreply.github.com>
2026-03-17 19:21:48 +0000

    Chore: helper coments

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 
-function App() {
 
+
+//Exemplo de um componente basico
+function App() {
+  return(<header>
+    <h1>Ribas Ferias</h1>
+  </header>)
 }
 
+// Isto serve de quando for importado em algum outro sitio, automaticamente assume este nome
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+//Onde comeca o "react" assim por dizer, neste caso ta a mandar o componente App so, isto é JSX 
+//Entao podem usar Javascript e Html ao mesmo tempo no mesmo ficheiro
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
