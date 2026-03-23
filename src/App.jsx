612a3b7f2d044615ac29b6d59a6a4022c20f8e18
AMPR2004 <alexandrerodrigues2004@gmail.com>
2026-03-23 14:56:52 +0000

    Feature: add login page and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home.jsx';
+import Login from './pages/login.jsx';
 
-
-
-//Exemplo de um componente basico
+//função que baseado na rota mostra um dos layouts
 function App() {
-  return(<header>
-    <h1>Ribas Ferias</h1>
-  </header>)
+  return (
+    <Router>
+      <Routes>
+        {/* Rota para a página de Login */}
+        <Route path="/" element={<Login />} />
+
+        {/* Rota para a Home (Página Principal) */}
+        <Route path="/home" element={<Home />} />
+        
+        {/*  adicionar outras rotas aqui conforme sao criados ficheiros nas pages */}
+      </Routes>
+    </Router>
+  );
 }
+
 
 // Isto serve de quando for importado em algum outro sitio, automaticamente assume este nome
 export default App
---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import MainLayout from '../layouts/main-layout';
+
+function Home() {
+  return (
+    <MainLayout>
+      <main>
+        <h1>Ribas Férias</h1>
+      </main>
+    </MainLayout>
+  );
+}
+
+export default Home;
